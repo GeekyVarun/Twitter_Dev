9f7dbbb96910cb586b86b5b5a72e075eb061ec3b
Varun <vgupta168@gmail.com>
2023-07-24 19:18:23 +0530

    Moved to es modules

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,16 @@
-const mongoose=require('mongoose');
+import mongoose from 'mongoose';
 
-const connect = async () => {
-    await mongoose
-    .connect("mongodb://127.0.0.1:27017/twitter_Dev")
-    .then(() => console.log("MongoDB Connected"))
-    .catch((err) => console.log("Mongo Error",err));
+
+// const connect = async () => {
+//     await mongoose
+//     .connect("mongodb://127.0.0.1:27017/twitter_Dev")
+//     .then(() => console.log("MongoDB Connected"))
+//     .catch((err) => console.log("Mongo Error",err));
+// }
+
+// module.exports=connect;
+
+export const connect = async () => {
+    await mongoose.connect("mongodb://127.0.0.1:27017/twitter_Dev");
 }
 
-module.exports=connect;
